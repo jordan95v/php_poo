410fcd1ec8b488d08ab75196201217955a61282b
jordan95v <jordan.dfrsne@gmail.com>
2022-12-30 18:48:23 +0100

    Started the main script, still need to create the class.

--- index.php
+++ index.php
@@ -9,11 +9,45 @@ use Classes\Characters\Wizard;
 use Classes\Enum\Type;
 use Classes\Spell\FireBall;
 
+while (true){
+    echo "Hello adventurer, please select your character's class !" . PHP_EOL;
+    echo "1 - Soldier" . PHP_EOL . "2 - Thief" . PHP_EOL . "3 - Wizard" . PHP_EOL;
+    $class_choices = readline("Enter your choice: ");
+    
+    if ($class_choices <= 3 && $class_choices > 0)
+    {
+        echo PHP_EOL;
+        break;
+    }
+    echo PHP_EOL . "Please select a correct answer." . PHP_EOL;
+}
 
-$fireBall = new FireBall();
-$soldier = new Thief($fireBall, null, null, Type::FIRE);
-$secondSoldier = new Thief($fireBall, null, null, Type::PLANT);
+while (true)
+{
+    echo "Great, now select your type !" . PHP_EOL;
+    echo "1 - Fire" . PHP_EOL . "2 - Water" . PHP_EOL . "3 - Plant" . PHP_EOL;
+    $class_type = readline("Enter your choice: ");
 
-$soldier->attack($secondSoldier);
+    if ($class_type <= 3 && $class_type > 0)
+    {
+        break;
+    }
+    echo PHP_EOL . "Please select a correct answer." . PHP_EOL;
+}
 
-echo $secondSoldier->getHealth();
+switch ($class_type)
+{
+    case 1:
+        $type = Type::FIRE;
+        break;
+    case 2:
+        $type = Type::WATER;
+        break;
+    case 3:
+        $type = Type::PLANT;
+        break;
+    default:
+        echo "Please select a correct answer." . PHP_EOL;
+        break;
+}
+    
